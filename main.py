06abf1ef914754817712b72220ae69ca0682a8b4
Pavel Vergeev <vernier_06_curios@icloud.com>
2023-06-29 21:25:57 +0300

    feat: add precode

---
+++ main.py
@@ -0,0 +1,30 @@
+# Разместите здесь импорты
+
+kitchen_chat_id = ...
+menu_url = ...
+
+
+updater = ...
+
+
+def get_menu():
+    ...
+
+
+def show_menu(update, context):
+    ...
+
+
+def process_order(update, context):
+    ...
+
+def wake_up(update, context):
+    ...
+
+
+# Здесь добавьте обработчиков:
+# * для команды 'start' — чтобы вызывалась функция wake_up
+# * для команды 'menu' — чтобы вызывалась функция show_menu
+# * для произвольного текста — чтобы вызывалась функция process_order
+
+# Запустите polling
